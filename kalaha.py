9c2a60f27da6034eeade18b4579f579c41e568a6
tex_479 <tex_gaming@web.de>
2024-12-31 22:48:50 +0100

    fix bug in `make_move()`

--- kalaha.py
+++ kalaha.py
@@ -32,20 +32,20 @@ def print_board(board:list[int]) -> None:
 
 def make_move(board:list[int], move:int) -> tuple[list[int], bool] | None:
     board_ret = board.copy()
-    if board[move] <= 0: return None
-    amount = board[move]
+    if board_ret[move] <= 0: return None
+    amount = board_ret[move]
     board_ret[move] = 0
     i = 0
-    index = 0
+    board_index = 0
     enemies_safe = 6 if move < 6 else 13
     while amount > 0:
         i += 1
         if ((move + i) % 14) == enemies_safe: i += 1
-        index = (move + i) % 14
-        board_ret[index] += 1
+        board_index = (move + i) % 14
+        board_ret[board_index] += 1
         amount -= 1
-    move_again = (index) == (enemies_safe - 7) % 14
-    final_hole = (index) % 14
+    move_again = (board_index) == (enemies_safe - 7) % 14
+    final_hole = (board_index) % 14
     if enemies_safe == 6:
         if final_hole < 6 and board_ret[final_hole] == 1:
             hole_mirrored = 12 - final_hole
@@ -62,10 +62,10 @@ def make_move(board:list[int], move:int) -> tuple[list[int], bool] | None:
                 board_ret[hole_mirrored] = 0
     
     if sum(board_ret[:6]) == 0:
-        board_ret[6] = sum(board_ret[7:13])
+        board_ret[6] += sum(board_ret[7:13])
         board_ret = [board_ret[i] if i in [6, 13] else 0 for i in range(14)]
     elif sum(board_ret[7:13]) == 0:
-        board_ret[13] = sum(board_ret[:6])
+        board_ret[13] += sum(board_ret[:6])
         board_ret = [board_ret[i] if i in [6, 13] else 0 for i in range(14)]
 
     return board_ret, move_again
